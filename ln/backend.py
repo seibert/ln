90dfb5d7ec6b966ecc06fe97c4a2101b37d33f82
Andy Mastbaum <amastbaum@gmail.com>
2013-08-21 13:48:33 -0400

    start implementing rest api with stubbed backend

--- ln/backend.py
+++ ln/backend.py
@@ -1,11 +1,66 @@
 '''Storage backends.'''
 
+class DataSource(object):
+    '''A source of data to log.'''
+    def __init__(self, name, type, reduction, interpolation, unit,
+            description):
+        self.name = name
+        self.type = type
+        self.reduction = reduction
+        self.interpolation = interpolation
+        self.unit = unit
+        self.description = description
+
+
+class Backend(object):
+    '''A storage backend interface.'''
+    def __init__(self):
+        pass
+
+    def get_data_source_list(self):
+        raise Exception('Cannot call get_data_source_list on base class.')
+
+    def create_data_source(self, name, type, reduction, interpolation, unit,
+            description):
+        raise Exception('Cannot call create_data_source on base class.')
+
+
+class DictBackend(Backend):
+    '''A storage backend implemented with a dict.
+    
+    For development use only!
+    '''
+    def __init__(self):
+        self.data = {}
+
+    def get_data_source_list(self):
+        return self.data.keys()
+
+    def create_data_source(self, name, type, reduction, interpolation, unit,
+            description):
+        self.data[name] = DataSource(name, type, reduction, interpolation,
+                                     unit, description)
+
+
 def init(config):
-    '''Initialize the storage backend.'''
+    '''Initialize the storage backend.
+    
+    :param config: Configuration definition
+    '''
     storage = config['storage']
     print 'Initializing "%s" storage backend...' % storage['backend']
 
     # do the stuff!
 
     print 'Done.'
+
+
+def get_backend(storage_config):
+    '''Find/build a backend based on the configuration.
+    
+    :param config: storage section of the configuration
+    '''
+    storage_type = storage_config['backend']
+    if storage_type == 'memory':
+        return DictBackend()
 
--- ln/server.py
+++ ln/server.py
@@ -1,21 +1,49 @@
 '''Web server functions.'''
 
+import json
+from flask import Flask
+from flask import request
 from ln import __version__
+from ln import backend
+
+app = Flask('ln')
+storage_backend = None
+
+@app.route('/')
+def root():
+    '''Get a list of data sources.'''
+    data = {
+        'names': storage_backend.get_data_source_list()
+    }
+
+    return json.dumps(data)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    '''Create a new data source.'''
+    f = request.form
+    storage_backend.create_data_source(f['name'], f['type'], f['reduction'],
+                                       f['interpolation'], f['unit'],
+                                       f['description'])
+
+    return ''
+
 
 def start(config):
-    '''Start the web server.'''
+    '''Start the web server.
+    
+    :param config: Configuration definition
+    '''
+    global storage_backend 
+
     print 'Natural log', __version__
 
     storage = config['storage']
     print 'Opening "%s" storage backend...' % storage['backend']
-    # open the backend
-    backend = None
+    storage_backend = backend.get_backend(storage)
 
     print 'Base URL is', config['url_base']
 
-    serve_forever(config['host'], config['port'], backend)
-
-
-def serve_forever(host, port, backend):
-    print 'Listening on %s:%i' % (host, port)
+    app.run(debug=True)
 
