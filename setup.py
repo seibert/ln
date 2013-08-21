60205c88a771c5c05e2d03547e6dfde1b0dc8706
Stan Seibert <stan@mtrr.org>
2013-08-21 14:27:43 -0400

    Make argparse a dependency if on python 2.6

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ from ln import __version__
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+install_requires = ['flask', 'requests', 'pytest']
+# Trick lifted from tox setup.py
+version = sys.version_info[:2]
+if version < (2,7) or (3,0) <= version <= (3,1):
+    install_requires.append('argparse')
+
 setup(
     name = "ln",
     version = __version__,
